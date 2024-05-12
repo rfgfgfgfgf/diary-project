01cce9b4d24ee41c6788be7b9f09538191356af9
Teymur <156570166+rfgfgfgfgf@users.noreply.github.com>
2024-05-12 10:06:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,22 +9,31 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #Создание db
-db = SQLAlchemy(app )
-
-#Задание №1. Создай таблицу БД
+db = SQLAlchemy(app)
+#Создание таблицы
 
 class Card(db.Model):
-    id = db.Column(db.Integer, nullable=False, primary_key=True)
-
+    #Создание полей
+    #id
+    id = db.Column(db.Integer, primary_key=True)
+    #Заголовок
     title = db.Column(db.String(100), nullable=False)
-
-    subtitle = db.Column(db.String(100), nullable=False)
-    
+    #Описание
+    subtitle = db.Column(db.String(300), nullable=False)
+    #Текст
     text = db.Column(db.Text, nullable=False)
 
+    #Вывод объекта и id
     def __repr__(self):
-            return f'<Card {self.id}>'
+        return f'<Card {self.id}>'
     
+
+#Задание №1. Создать таблицу User
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    login = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+
 
 
 
@@ -32,21 +41,55 @@ class Card(db.Model):
 
 
 #Запуск страницы с контентом
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
+def login():
+        error = ''
+        if request.method == 'POST':
+            form_login = request.form['email']
+            form_password = request.form['password']
+            
+            #Задание №4. Реализовать проверку пользователей
+            users_db = User.query.all()
+            for user in users_db:
+                if form_login == user.login and form_password == user.password:
+                    return redirect('/index')
+            else:
+                error = 'Неправильно указан пользователь или пароль'
+                return render_template('login.html', error=error)
+
+            
+        else:
+            return render_template('login.html')
+
+
+
+@app.route('/reg', methods=['GET','POST'])
+def reg():
+    if request.method == 'POST':
+        login= request.form['email']
+        password = request.form['password']
+        
+        #Задание №3. Реализовать запись пользователей
+        user = User(login=login, password=password)
+        db.session.add(user)
+        db.session.commit()
+        
+        return redirect('/')
+    
+    else:    
+        return render_template('registration.html')
+
+
+#Запуск страницы с контентом
+@app.route('/index')
 def index():
     #Отображение объектов из БД
-    #Задание №2. Отоброзить объекты из БД в index.html
     cards = Card.query.order_by(Card.id).all()
-
-    return render_template('index.html',
-                           cards = cards,
-
-                           )
+    return render_template('index.html', cards=cards)
 
 #Запуск страницы c картой
 @app.route('/card/<int:id>')
 def card(id):
-    #Задание №2. Отоброзить нужную карточку по id
     card = Card.query.get(id)
 
     return render_template('card.html', card=card)
@@ -64,11 +107,22 @@ def form_create():
         subtitle =  request.form['subtitle']
         text =  request.form['text']
 
-        #Задание №2. Создайте сопосб записи данных в БД
-        
+        #Создание объкта для передачи в дб
+
         card = Card(title=title, subtitle=subtitle, text=text)
+
         db.session.add(card)
         db.session.commit()
+        return redirect('/index')
+    else:
+        return render_template('create_card.html')
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
         
 
 
